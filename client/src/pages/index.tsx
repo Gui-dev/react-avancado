730554ec34b3b1a94056f955d26a26e7a09f6575
Gui-dev <adrianon10@gmail.com>
2021-02-22 04:01:15 -0300

    feat: add page main

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,9 +5,10 @@ const Title = styled.h1`
   font-size: 50px;
 `
 
-export default function Home () {
-
-  return(
-    <Title>Hello World</Title>
+export default function Home() {
+  return (
+    <>
+      <Title>Hello World</Title>
+    </>
   )
 }
