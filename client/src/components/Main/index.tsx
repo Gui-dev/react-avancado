e33d4d1f9714abdf96c32ddc8a881cc2304236b7
Gui-dev <adrianon10@gmail.com>
2021-03-08 03:05:06 -0300

    feat: add page Main

--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,9 +1,20 @@
-// import { Container } from './style';
+import { Container, Logo, Title, Description, Illustration } from './style'
 
 export const Main: React.FC = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <Container>
+      <Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado"
+        title="Logo"
+      />
+      <Title>React Avançado</Title>
+      <Description>Typescript, ReactJS, NextJS e Styled Components</Description>
+      <Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para a tela com código"
+        title="illustration"
+      />
+    </Container>
   )
 }
